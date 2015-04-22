92ce0792ec307abc5f5b814213077a6b7ba7611a
nheyn <nick@heyn.io>
2015-04-22 16:51:15 -0600

    Update Error Messages

--- __tests__/dispatcher-test.js
+++ __tests__/dispatcher-test.js
@@ -1,30 +1,43 @@
-/*jest.dontMock("babel/polyfill");
+/*
+jest.dontMock("babel/polyfill");
 require("babel/polyfill");
 
-$ docker run -v "$(pwd)"/src:/var/www/src -v "$(pwd)"/__tests__:/var/www/__tests__ -it --rm dispatcher jest
+/*
+$ docker run -v "$(pwd)"/src:/var/www/src -v "$(pwd)"/__tests__:/var/www/__tests__ -it --rm dispatcher npm test
+
+> dispatcher@0.1.0 test /var/www
+> jest
+
 Using Jest CLI v0.4.0
  FAIL  __tests__/dispatcher-test.js
 TypeError: /var/www/__tests__/dispatcher-test.js: /var/www/node_modules/babel/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/lib/babel/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/shim.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/es5.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/$.cof.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/$.wks.js: Cannot read property 'g' of undefined
 1 test failed, 0 tests passed (1 total)
-Run time: 2.819s
+Run time: 2.822s
+npm ERR! Test failed.  See above for more details.
 */
 
 
-/*jest.dontMock('es6-shim');
+/*
+jest.dontMock('es6-shim');
 import 'es6-shim';
 
+/*
 $ docker run -v "$(pwd)"/src:/var/www/src -v "$(pwd)"/__tests__:/var/www/__tests__ -it --rm dispatcher bash
-root@705fd1e337f3:/var/www# npm install es6-shim
+root@379c06fd7b42:/var/www# npm install es6-shim
 npm WARN package.json dispatcher@0.1.0 No repository field.
 npm WARN package.json dispatcher@0.1.0 No README data
-npm WARN package.json flow@0.2.3 No repository field.
 es6-shim@0.28.1 node_modules/es6-shim
-root@705fd1e337f3:/var/www# jest
+root@379c06fd7b42:/var/www# npm test
+
+> dispatcher@0.1.0 test /var/www
+> jest
+
 Using Jest CLI v0.4.0
  FAIL  __tests__/dispatcher-test.js
 TypeError: /var/www/__tests__/dispatcher-test.js: /var/www/node_modules/es6-shim/es6-shim.js: Object #<Map> has no method 'keys'
 1 test failed, 0 tests passed (1 total)
-Run time: 2.663s
+Run time: 2.78s
+npm ERR! Test failed.  See above for more details.
 */
 
 jest.dontMock('../src/dispatcher.js');
