3ec7df152ba838f32347366d5fb6697ba7f9b5fe
nheyn <nick@heyn.io>
2015-04-23 15:53:50 -0600

    Add Unregister Test
    (Test still not running because of polyfill issue)

--- __tests__/dispatcher-test.js
+++ __tests__/dispatcher-test.js
@@ -1,69 +1,51 @@
-/*
 jest.dontMock("babel/polyfill");
 require("babel/polyfill");
-
-/*
-$ docker run -v "$(pwd)"/src:/var/www/src -v "$(pwd)"/__tests__:/var/www/__tests__ -it --rm dispatcher npm test
-
-> dispatcher@0.1.0 test /var/www
-> jest
-
-Using Jest CLI v0.4.0
- FAIL  __tests__/dispatcher-test.js
-TypeError: /var/www/__tests__/dispatcher-test.js: /var/www/node_modules/babel/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/lib/babel/polyfill.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/shim.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/es5.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/$.cof.js: /var/www/node_modules/babel/node_modules/babel-core/node_modules/core-js/modules/$.wks.js: Cannot read property 'g' of undefined
-1 test failed, 0 tests passed (1 total)
-Run time: 2.822s
-npm ERR! Test failed.  See above for more details.
-*/
-
-
 /*
 jest.dontMock('es6-shim');
 import 'es6-shim';
-
-/*
-$ docker run -v "$(pwd)"/src:/var/www/src -v "$(pwd)"/__tests__:/var/www/__tests__ -it --rm dispatcher bash
-root@379c06fd7b42:/var/www# npm install es6-shim
-npm WARN package.json dispatcher@0.1.0 No repository field.
-npm WARN package.json dispatcher@0.1.0 No README data
-es6-shim@0.28.1 node_modules/es6-shim
-root@379c06fd7b42:/var/www# npm test
-
-> dispatcher@0.1.0 test /var/www
-> jest
-
-Using Jest CLI v0.4.0
- FAIL  __tests__/dispatcher-test.js
-TypeError: /var/www/__tests__/dispatcher-test.js: /var/www/node_modules/es6-shim/es6-shim.js: Object #<Map> has no method 'keys'
-1 test failed, 0 tests passed (1 total)
-Run time: 2.78s
-npm ERR! Test failed.  See above for more details.
-*/
+//*/
 
 jest.dontMock('../src/dispatcher.js');
 
 describe('Dispatcher', function() {
 	it('registers callbacks', function() {
-		/*var dispatcher = makeEmptyDispatcher();
-		var callbacks = makeTestCallbacks();*/
+		var dispatcher = makeEmptyDispatcher();
+		var callbacks = makeTestCallbacks();
 
 		var symCallbackMap = new Map();
-		/*for(var i=0; i<callbacks.length; i++) {
-			var newSym = dispatcher.register(callbacks[i]);
-
+		registerCallbacks(dispatcher, callbacks, function(newSym, i) {
 			// Check symbol is unique
 			expect(symCallbackMap.has(newSym)).toBe(false);
-
 			symCallbackMap.set(newSym, i);
-		}*/
+		});
 
-		symCallbackMap.forEach((callbackIndex, sym) => {/*
+		symCallbackMap.forEach((callbackIndex, sym) => {
 			// Check symbol has been added to dispatcher
 			expect(dispatcher._callbacks.has(sym)).toBe(true);
 
 			// Check symbol has the correct callback
-			expect(dispatcher._callbacks.get(sym)).toBe(callback[callbackIndex]);*/
+			expect(dispatcher._callbacks.get(sym)).toBe(callback[callbackIndex]);
 		});
+	});
+
+	it('unregisters callbacks', function() {
+		var dispatcher = makeEmptyDispatcher();
+		var callbacks = makeTestCallbacks();
+		var symSet = new Set();
+		registerCallbacks(dispatcher, callbacks, (newSym) => symCallbackMap.add(newSym));
+	
+		symSet.forEach((sym) => {
+			var didRemove = dispatcher.unregister(sym);
+
+			// Check unregister returns true when valid sym is removed
+			expect(didRemove).toBe(true);
+
+			// Check unregistered callback has been removed from the dispatcher
+			expect(dispatcher._callbacks.has(sym)).toBe(false);
+		});
+
+		// Check
+		expect(dispatcher.unregister(Symbol()).toBe(false)
 	});
 });
 
@@ -81,4 +63,12 @@ function makeTestCallbacks() {
 		(payload) => payload.b? payload: {},
 		(payload) => payload.c? payload: Promise.resolve(payload)
 	];
+}
+
+function registerCallbacks(dispatcher, callbacks, onAddCallback) {
+	for(var i=0; i<callbacks.length; i++) {
+		var newSym = dispatcher.register(callbacks[i]);
+		
+		if(onAddCallback) onAddCallback(newSym, i);
+	}
 }		
